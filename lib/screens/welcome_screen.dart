6abf37eed7fc2512ff4af45c6a8bd2d72c772c7e
Montuuh <dmontufo@gmail.com>
2021-12-15 18:23:35 +0100

    welcome screen. Now firebase doenst work??????????

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 50,
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text("Sign Up"),
+        ),
+      ],
+    );
+  }
+}
