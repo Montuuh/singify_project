a3d46ac00e4a87e8d0189110d8895fc5e986eed8
Montuuh <dmontufo@gmail.com>
2021-12-22 11:56:53 +0100

    Authenticator done (google auth not working)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:singify_project/widgets/auth_gate.dart';
 import 'package:singify_project/widgets/screen_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const SingifyApp());
+  // await dotenv.load(fileName: '.env');
+
+  runApp(
+    const AuthGate(
+      app: SingifyApp(),
+    ),
+  );
 }
 
 class SingifyApp extends StatelessWidget {
