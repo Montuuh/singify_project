a70db956ab59751c89d8ca7a03ae77a6c2533917
Montuuh <dmontufo@gmail.com>
2021-12-22 12:28:21 +0100

    render auth email and log out button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Text(user.email.toString()),
+            IconButton(
+              icon: const Icon(Icons.logout),
+              tooltip: 'Log out',
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
